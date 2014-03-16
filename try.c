c40dbe991ad1d23a6a9904b51db50dde64dbcfa7
timmy00274672 <timmy00274672@gmail.com>
2014-03-16 23:00:44 +0800

    Try standard list of the kernel.
    Also expand the macro.

---
+++ try.c
@@ -0,0 +1,7 @@
+#include <linux/list.h>
+
+int main(int argc, char const *argv[])
+{
+	LIST_HEAD(hi);
+	return 0;
+}
---
+++ try_expand.c
@@ -0,0 +1,695 @@
+# 1 "try.c"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 30 "/usr/include/stdc-predef.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/bits/predefs.h" 1 3 4
+# 31 "/usr/include/stdc-predef.h" 2 3 4
+# 1 "<command-line>" 2
+# 1 "try.c"
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h" 1
+
+
+
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/types.h" 1
+
+
+
+
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/linux/types.h" 1
+
+
+
+# 1 "/usr/include/x86_64-linux-gnu/asm/types.h" 1 3 4
+
+
+
+# 1 "/usr/include/asm-generic/types.h" 1 3 4
+
+
+
+
+
+
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/asm-generic/int-ll64.h" 1 3 4
+# 10 "/usr/src/linux-headers-3.11.0-12-generic/include/asm-generic/int-ll64.h" 3 4
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/asm-generic/int-ll64.h" 1 3 4
+# 11 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/asm-generic/int-ll64.h" 3 4
+# 1 "/usr/include/x86_64-linux-gnu/asm/bitsperlong.h" 1 3 4
+# 10 "/usr/include/x86_64-linux-gnu/asm/bitsperlong.h" 3 4
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/asm-generic/bitsperlong.h" 1 3 4
+
+
+
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/asm-generic/bitsperlong.h" 1 3 4
+# 5 "/usr/src/linux-headers-3.11.0-12-generic/include/asm-generic/bitsperlong.h" 2 3 4
+# 11 "/usr/include/x86_64-linux-gnu/asm/bitsperlong.h" 2 3 4
+# 12 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/asm-generic/int-ll64.h" 2 3 4
+
+
+
+
+
+
+
+typedef __signed__ char __s8;
+typedef unsigned char __u8;
+
+typedef __signed__ short __s16;
+typedef unsigned short __u16;
+
+typedef __signed__ int __s32;
+typedef unsigned int __u32;
+
+
+__extension__ typedef __signed__ long long __s64;
+__extension__ typedef unsigned long long __u64;
+# 11 "/usr/src/linux-headers-3.11.0-12-generic/include/asm-generic/int-ll64.h" 2 3 4
+
+
+
+
+typedef signed char s8;
+typedef unsigned char u8;
+
+typedef signed short s16;
+typedef unsigned short u16;
+
+typedef signed int s32;
+typedef unsigned int u32;
+
+typedef signed long long s64;
+typedef unsigned long long u64;
+# 8 "/usr/include/asm-generic/types.h" 2 3 4
+# 5 "/usr/include/x86_64-linux-gnu/asm/types.h" 2 3 4
+# 5 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/linux/types.h" 2
+# 13 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/linux/types.h"
+# 1 "/usr/include/linux/posix_types.h" 1 3 4
+
+
+
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/stddef.h" 1 3 4
+
+
+
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/linux/stddef.h" 1 3 4
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/compiler.h" 1 3 4
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/linux/stddef.h" 2 3 4
+# 5 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/stddef.h" 2 3 4
+
+
+
+
+
+enum {
+ false = 0,
+ true = 1
+};
+# 5 "/usr/include/linux/posix_types.h" 2 3 4
+# 24 "/usr/include/linux/posix_types.h" 3 4
+typedef struct {
+ unsigned long fds_bits[1024 / (8 * sizeof(long))];
+} __kernel_fd_set;
+
+
+typedef void (*__kernel_sighandler_t)(int);
+
+
+typedef int __kernel_key_t;
+typedef int __kernel_mqd_t;
+
+# 1 "/usr/include/x86_64-linux-gnu/asm/posix_types.h" 1 3 4
+
+
+
+
+
+# 1 "/usr/include/x86_64-linux-gnu/asm/posix_types_64.h" 1 3 4
+# 10 "/usr/include/x86_64-linux-gnu/asm/posix_types_64.h" 3 4
+typedef unsigned short __kernel_old_uid_t;
+typedef unsigned short __kernel_old_gid_t;
+
+
+typedef unsigned long __kernel_old_dev_t;
+
+
+# 1 "/usr/include/asm-generic/posix_types.h" 1 3 4
+# 14 "/usr/include/asm-generic/posix_types.h" 3 4
+typedef long __kernel_long_t;
+typedef unsigned long __kernel_ulong_t;
+
+
+
+typedef __kernel_ulong_t __kernel_ino_t;
+
+
+
+typedef unsigned int __kernel_mode_t;
+
+
+
+typedef int __kernel_pid_t;
+
+
+
+typedef int __kernel_ipc_pid_t;
+
+
+
+typedef unsigned int __kernel_uid_t;
+typedef unsigned int __kernel_gid_t;
+
+
+
+typedef __kernel_long_t __kernel_suseconds_t;
+
+
+
+typedef int __kernel_daddr_t;
+
+
+
+typedef unsigned int __kernel_uid32_t;
+typedef unsigned int __kernel_gid32_t;
+# 71 "/usr/include/asm-generic/posix_types.h" 3 4
+typedef __kernel_ulong_t __kernel_size_t;
+typedef __kernel_long_t __kernel_ssize_t;
+typedef __kernel_long_t __kernel_ptrdiff_t;
+
+
+
+
+typedef struct {
+ int val[2];
+} __kernel_fsid_t;
+
+
+
+
+
+typedef __kernel_long_t __kernel_off_t;
+typedef long long __kernel_loff_t;
+typedef __kernel_long_t __kernel_time_t;
+typedef __kernel_long_t __kernel_clock_t;
+typedef int __kernel_timer_t;
+typedef int __kernel_clockid_t;
+typedef char * __kernel_caddr_t;
+typedef unsigned short __kernel_uid16_t;
+typedef unsigned short __kernel_gid16_t;
+# 18 "/usr/include/x86_64-linux-gnu/asm/posix_types_64.h" 2 3 4
+# 7 "/usr/include/x86_64-linux-gnu/asm/posix_types.h" 2 3 4
+# 36 "/usr/include/linux/posix_types.h" 2 3 4
+# 14 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/linux/types.h" 2
+# 32 "/usr/src/linux-headers-3.11.0-12-generic/include/uapi/linux/types.h"
+typedef __u16 __le16;
+typedef __u16 __be16;
+typedef __u32 __le32;
+typedef __u32 __be32;
+typedef __u64 __le64;
+typedef __u64 __be64;
+
+typedef __u16 __sum16;
+typedef __u32 __wsum;
+# 6 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/types.h" 2
+
+
+
+
+
+
+typedef __u32 __kernel_dev_t;
+
+typedef __kernel_fd_set fd_set;
+typedef __kernel_dev_t dev_t;
+typedef __kernel_ino_t ino_t;
+typedef __kernel_mode_t mode_t;
+typedef unsigned short umode_t;
+typedef __u32 nlink_t;
+typedef __kernel_off_t off_t;
+typedef __kernel_pid_t pid_t;
+typedef __kernel_daddr_t daddr_t;
+typedef __kernel_key_t key_t;
+typedef __kernel_suseconds_t suseconds_t;
+typedef __kernel_timer_t timer_t;
+typedef __kernel_clockid_t clockid_t;
+typedef __kernel_mqd_t mqd_t;
+
+typedef _Bool bool;
+
+typedef __kernel_uid32_t uid_t;
+typedef __kernel_gid32_t gid_t;
+typedef __kernel_uid16_t uid16_t;
+typedef __kernel_gid16_t gid16_t;
+
+typedef unsigned long uintptr_t;
+# 45 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/types.h"
+typedef __kernel_loff_t loff_t;
+# 54 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/types.h"
+typedef __kernel_size_t size_t;
+
+
+
+
+typedef __kernel_ssize_t ssize_t;
+
+
+
+
+typedef __kernel_ptrdiff_t ptrdiff_t;
+
+
+
+
+typedef __kernel_time_t time_t;
+
+
+
+
+typedef __kernel_clock_t clock_t;
+
+
+
+
+typedef __kernel_caddr_t caddr_t;
+
+
+
+typedef unsigned char u_char;
+typedef unsigned short u_short;
+typedef unsigned int u_int;
+typedef unsigned long u_long;
+
+
+typedef unsigned char unchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+
+
+
+typedef __u8 u_int8_t;
+typedef __s8 int8_t;
+typedef __u16 u_int16_t;
+typedef __s16 int16_t;
+typedef __u32 u_int32_t;
+typedef __s32 int32_t;
+
+
+
+typedef __u8 uint8_t;
+typedef __u16 uint16_t;
+typedef __u32 uint32_t;
+
+
+typedef __u64 uint64_t;
+typedef __u64 u_int64_t;
+typedef __s64 int64_t;
+# 133 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/types.h"
+typedef unsigned long sector_t;
+typedef unsigned long blkcnt_t;
+# 148 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/types.h"
+typedef u32 dma_addr_t;
+# 157 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/types.h"
+typedef unsigned gfp_t;
+typedef unsigned fmode_t;
+typedef unsigned oom_flags_t;
+
+
+
+
+typedef u32 phys_addr_t;
+
+
+typedef phys_addr_t resource_size_t;
+
+
+
+
+
+typedef unsigned long irq_hw_number_t;
+
+typedef struct {
+ int counter;
+} atomic_t;
+
+
+
+
+
+
+
+struct list_head {
+ struct list_head *next, *prev;
+};
+
+struct hlist_head {
+ struct hlist_node *first;
+};
+
+struct hlist_node {
+ struct hlist_node *next, **pprev;
+};
+
+struct ustat {
+ __kernel_daddr_t f_tfree;
+ __kernel_ino_t f_tinode;
+ char f_fname[6];
+ char f_fpack[6];
+};
+
+
+
+
+
+
+struct callback_head {
+ struct callback_head *next;
+ void (*func)(struct callback_head *head);
+};
+# 5 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h" 2
+
+# 1 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/poison.h" 1
+# 7 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h" 2
+# 1 "/usr/include/linux/const.h" 1 3 4
+# 8 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h" 2
+# 24 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+ list->next = list;
+ list->prev = list;
+}
+# 37 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void __list_add(struct list_head *new,
+         struct list_head *prev,
+         struct list_head *next)
+{
+ next->prev = new;
+ new->next = next;
+ new->prev = prev;
+ prev->next = new;
+}
+# 60 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+ __list_add(new, head, head->next);
+}
+# 74 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+ __list_add(new, head->prev, head);
+}
+# 86 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+ next->prev = prev;
+ prev->next = next;
+}
+# 99 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void __list_del_entry(struct list_head *entry)
+{
+ __list_del(entry->prev, entry->next);
+}
+
+static inline void list_del(struct list_head *entry)
+{
+ __list_del(entry->prev, entry->next);
+ entry->next = ((void *) 0x00100100 + 0);
+ entry->prev = ((void *) 0x00200200 + 0);
+}
+# 122 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void list_replace(struct list_head *old,
+    struct list_head *new)
+{
+ new->next = old->next;
+ new->next->prev = new;
+ new->prev = old->prev;
+ new->prev->next = new;
+}
+
+static inline void list_replace_init(struct list_head *old,
+     struct list_head *new)
+{
+ list_replace(old, new);
+ INIT_LIST_HEAD(old);
+}
+
+
+
+
+
+static inline void list_del_init(struct list_head *entry)
+{
+ __list_del_entry(entry);
+ INIT_LIST_HEAD(entry);
+}
+
+
+
+
+
+
+static inline void list_move(struct list_head *list, struct list_head *head)
+{
+ __list_del_entry(list);
+ list_add(list, head);
+}
+
+
+
+
+
+
+static inline void list_move_tail(struct list_head *list,
+      struct list_head *head)
+{
+ __list_del_entry(list);
+ list_add_tail(list, head);
+}
+
+
+
+
+
+
+static inline int list_is_last(const struct list_head *list,
+    const struct list_head *head)
+{
+ return list->next == head;
+}
+
+
+
+
+
+static inline int list_empty(const struct list_head *head)
+{
+ return head->next == head;
+}
+# 204 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline int list_empty_careful(const struct list_head *head)
+{
+ struct list_head *next = head->next;
+ return (next == head) && (next == head->prev);
+}
+
+
+
+
+
+static inline void list_rotate_left(struct list_head *head)
+{
+ struct list_head *first;
+
+ if (!list_empty(head)) {
+  first = head->next;
+  list_move_tail(first, head);
+ }
+}
+
+
+
+
+
+static inline int list_is_singular(const struct list_head *head)
+{
+ return !list_empty(head) && (head->next == head->prev);
+}
+
+static inline void __list_cut_position(struct list_head *list,
+  struct list_head *head, struct list_head *entry)
+{
+ struct list_head *new_first = entry->next;
+ list->next = head->next;
+ list->next->prev = list;
+ list->prev = entry;
+ entry->next = list;
+ head->next = new_first;
+ new_first->prev = head;
+}
+# 259 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void list_cut_position(struct list_head *list,
+  struct list_head *head, struct list_head *entry)
+{
+ if (list_empty(head))
+  return;
+ if (list_is_singular(head) &&
+  (head->next != entry && head != entry))
+  return;
+ if (entry == head)
+  INIT_LIST_HEAD(list);
+ else
+  __list_cut_position(list, head, entry);
+}
+
+static inline void __list_splice(const struct list_head *list,
+     struct list_head *prev,
+     struct list_head *next)
+{
+ struct list_head *first = list->next;
+ struct list_head *last = list->prev;
+
+ first->prev = prev;
+ prev->next = first;
+
+ last->next = next;
+ next->prev = last;
+}
+
+
+
+
+
+
+static inline void list_splice(const struct list_head *list,
+    struct list_head *head)
+{
+ if (!list_empty(list))
+  __list_splice(list, head, head->next);
+}
+
+
+
+
+
+
+static inline void list_splice_tail(struct list_head *list,
+    struct list_head *head)
+{
+ if (!list_empty(list))
+  __list_splice(list, head->prev, head);
+}
+# 318 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void list_splice_init(struct list_head *list,
+        struct list_head *head)
+{
+ if (!list_empty(list)) {
+  __list_splice(list, head, head->next);
+  INIT_LIST_HEAD(list);
+ }
+}
+# 335 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void list_splice_tail_init(struct list_head *list,
+      struct list_head *head)
+{
+ if (!list_empty(list)) {
+  __list_splice(list, head->prev, head);
+  INIT_LIST_HEAD(list);
+ }
+}
+# 570 "/usr/src/linux-headers-3.11.0-12-generic/include/linux/list.h"
+static inline void INIT_HLIST_NODE(struct hlist_node *h)
+{
+ h->next = ((void *)0);
+ h->pprev = ((void *)0);
+}
+
+static inline int hlist_unhashed(const struct hlist_node *h)
+{
+ return !h->pprev;
+}
+
+static inline int hlist_empty(const struct hlist_head *h)
+{
+ return !h->first;
+}
+
+static inline void __hlist_del(struct hlist_node *n)
+{
+ struct hlist_node *next = n->next;
+ struct hlist_node **pprev = n->pprev;
+ *pprev = next;
+ if (next)
+  next->pprev = pprev;
+}
+
+static inline void hlist_del(struct hlist_node *n)
+{
+ __hlist_del(n);
+ n->next = ((void *) 0x00100100 + 0);
+ n->pprev = ((void *) 0x00200200 + 0);
+}
+
+static inline void hlist_del_init(struct hlist_node *n)
+{
+ if (!hlist_unhashed(n)) {
+  __hlist_del(n);
+  INIT_HLIST_NODE(n);
+ }
+}
+
+static inline void hlist_add_head(struct hlist_node *n, struct hlist_head *h)
+{
+ struct hlist_node *first = h->first;
+ n->next = first;
+ if (first)
+  first->pprev = &n->next;
+ h->first = n;
+ n->pprev = &h->first;
+}
+
+
+static inline void hlist_add_before(struct hlist_node *n,
+     struct hlist_node *next)
+{
+ n->pprev = next->pprev;
+ n->next = next;
+ next->pprev = &n->next;
+ *(n->pprev) = n;
+}
+
+static inline void hlist_add_after(struct hlist_node *n,
+     struct hlist_node *next)
+{
+ next->next = n->next;
+ n->next = next;
+ next->pprev = &n->next;
+
+ if(next->next)
+  next->next->pprev = &next->next;
+}
+
+
+static inline void hlist_add_fake(struct hlist_node *n)
+{
+ n->pprev = &n->next;
+}
+
+
+
+
+
+static inline void hlist_move_list(struct hlist_head *old,
+       struct hlist_head *new)
+{
+ new->first = old->first;
+ if (new->first)
+  new->first->pprev = &new->first;
+ old->first = ((void *)0);
+}
+# 2 "try.c" 2
+
+int main(int argc, char const *argv[])
+{
+ struct list_head hi = { &(hi), &(hi) };
+ return 0;
+}
